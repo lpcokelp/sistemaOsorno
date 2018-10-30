ddd33ad8ca5dc7cb8a2271c203ec7b98ffb0fe3c
Jorge Eduardo Morales Morales <jorge.morales@outlook.cl>
2018-10-30 00:21:47 -0300

    Revert "modificacion modulos administrador"
    This reverts commit 9c24348a13fd0bdab374d39e5afc32a78cb2ae96.

--- administrador/modulos/maquinas/js/maquinas.js
+++ administrador/modulos/maquinas/js/maquinas.js
@@ -88,13 +88,9 @@ function cargarmaquinas() {
             </tr>
             `
         })
-        $('#cuerpoMaquinas').html(contenidotablamaquina);
-        altura = parseInt(screen.height);
-        altura = altura * 0.6;
-        estilo = 'style="max-height: ' + altura + 'px; overflow-y: scroll;"';
-        $('#contenidoMaquinas').attr('style', estilo);
-    }).then(function() {
-        var tablaMaq =$('#tablaMaquinas').DataTable({
+        $('#listadoMaquinas').html(contenidotablamaquina);
+      
+        var tablaRec =$('#tablaMaquinas').DataTable({
             "pageLength": 7,
             orderCellsTop: true,
             fixedHeader: true,
@@ -131,8 +127,8 @@ function cargarmaquinas() {
             $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
      
             $( 'input', this ).on( 'keyup change', function () {
-                if ( tablaMaq.column(i).search() !== this.value ) {
-                    tablaMaq
+                if ( tablaRec.column(i).search() !== this.value ) {
+                    tablaRec
                         .column(i)
                         .search( this.value )
                         .draw();
