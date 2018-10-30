2aeb48ce3cdf615bba069dc11958cdd1bc0a0445
Jorge Eduardo Morales Morales <jorge.morales@outlook.cl>
2018-10-30 16:32:32 -0300

    cambios caja
    cambios modulo caja premios y recaudaciones

--- caja/modulos/caja/js/recaudaciones.js
+++ caja/modulos/caja/js/recaudaciones.js
@@ -210,52 +210,15 @@ function cargarRecaudaciones() {
         $('#cuerpoRecaudaciones').html(contenidoTablaRecaudaciones);
         $('#recaudacionTotal').html(puntos("" + montoTotalRecaudaciones + ""));
         $('#numeroTotalRecaudaciones').html(contadorRecaudacion);
-      
-        var tablaRec =$('#tablaRecaudaciones').DataTable({
-            "pageLength": 7,
-            orderCellsTop: true,
-            fixedHeader: true,
-            "searching": false,
-            language: {
-                "sProcessing": "Procesando...",
-                "sLengthMenu": "Mostrar _MENU_ registros",
-                "sZeroRecords": "No se encontraron resultados",
-                "sEmptyTable": "Ningún dato disponible en esta tabla",
-                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix": "",
-                "sSearch": "Buscar:",
-                "sUrl": "",
-                "sInfoThousands": ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst": "Primero",
-                    "sLast": "Último",
-                    "sNext": "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                }
-            }
-        });
+        cuerpoRecaudaciones = $('#cuerpoRecaudaciones').html();
+        cuerpoRecaudaciones += `<tr id="` + datoss.key + `">
+        <td>`+ maquina + `</td>
+        <td>`+ puntos(monto) + `</td>
+        <td>`+ obtenerHora() + `</td>
+        <td>  <i class="material-icons"  onclick="eliminarPremio('` + datoss.key + `','` + monto + `')">delete</i></td>
+        </tr>`;
+        $('#cuerpoRecaudaciones').html(cuerpoRecaudaciones);
 
-        $('#tablaRecaudaciones thead tr').clone(true).appendTo( '#tablaRecaudaciones thead' );
-        $('#tablaRecaudaciones thead tr:eq(1) th').each( function (i) {
-            var title = $(this).text();
-            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-     
-            $( 'input', this ).on( 'keyup change', function () {
-                if ( tablaRec.column(i).search() !== this.value ) {
-                    tablaRec
-                        .column(i)
-                        .search( this.value )
-                        .draw();
-                }
-            } );
-        } );
     })
 }
 
